f53efddcf398059a947999e08624454b2bfcbd3e
Amanda Simmons <carpe737diem@gmail.com>
2020-11-13 14:54:29 -0500

    added mike to references

--- lists.py
+++ lists.py
@@ -16,7 +16,7 @@ Kenzie assignment: Lists!
 
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "Amanda Simmons, Jonny, Geeksforgeeks, stackoverflow."
+__author__ = "Amanda Simmons, Jonny, Mike B, Geeksforgeeks, stackoverflow."
 __author__ = "Amanda Simmons, realpython, python docs, programiz,digitalocean."
 # A. match_ends
 # Given a list of strings, return the count of the number of
